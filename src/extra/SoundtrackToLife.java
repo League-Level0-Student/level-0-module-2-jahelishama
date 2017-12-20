3c2b7f1013fb60b765c862138451645d28549ae9
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2017-12-19 19:22:49 -0800

    sound track to life

--- src/extra/SoundtrackToLife.java
+++ src/extra/SoundtrackToLife.java
@@ -12,14 +12,23 @@ public class SoundtrackToLife {
 
 		// 1. Adjust this pop-up to find out what mood the user is in.
 		int userMood = JOptionPane.showOptionDialog(null, "The question", "Pop-up Title", 0,
-				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Mood1", "Mood2", "Mood3" }, null);
+				JOptionPane.INFORMATION_MESSAGE, null, new String[] { "Happy", "Angry", "Sad", "stressed" }, null);
 		
 		// 2. Their answer is stored in the userMood variable. Print it out.
-
+			System.out.println(userMood);
 		// 3. If they are in a stressed mood, use the playVideo method to play a calming song from YouTube.
-		
-
-		
+		if(userMood==1){
+			playVideo("https://www.youtube.com/watch?v=1Zbel8LF6nU");
+		}
+		if(userMood==2){
+			playVideo("https://www.youtube.com/watch?v=NaN9Blt5MGk");
+		}
+		if(userMood==0){
+			playVideo("https://www.youtube.com/watch?v=O_Wjh2qulgg");
+		}
+		if(userMood==3){
+			playVideo("https://www.youtube.com/watch?v=IgmaJC-Km5U");
+		}
 		// 4. Play different songs for other moods.
 
 /**
